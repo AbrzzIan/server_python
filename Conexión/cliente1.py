180b777b459f2caf091a36484d3fed9b8c170e3d
AbrzzIan <abruzzeseian@gmail.com>
2024-05-10 13:45:34 -0300

    first commit

---
+++ Conexión/cliente1.py
@@ -0,0 +1,50 @@
+import sys
+import socket
+
+TCP_IP = '127.0.0.1'
+TCP_PORT = 12345
+BUFFER_SIZE = 20
+MESSAGE = "Hola Mundo!"
+
+if len(sys.argv) >= 2:
+    TCP_IP = sys.argv[1]
+
+if len(sys.argv) >= 3:
+    MESSAGE = sys.argv[2]
+
+print ("[CLIENTE] Iniciando")
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+print ("[CLIENTE] Conectando")
+s.connect((TCP_IP, TCP_PORT))
+print ("[CLIENTE] soy el cliente: \"" + str(s.getsockname()) + "\"")
+
+def enviarMensaje():
+    msg = input("Mensaje a enviar: ")
+    print ("[CLIENTE] Enviando datos: \"" + msg + "\"")
+    s.send((msg + '\n').encode('utf-8'))   
+      
+
+
+print ("[CLIENTE] Recibiendo datos del SERVIDOR")
+msg = ''
+fin_msg = False
+datos = bytearray()
+while not fin_msg:
+	recvd = s.recv(BUFFER_SIZE)
+	datos += recvd
+	print ("[CLIENTE] Recibidos ", len(recvd), " bytes")
+	if b'\n' in recvd:
+		msg = datos.rstrip(b'\n').decode('utf-8')
+		fin_msg = True
+            
+print ("[CLIENTE] Recibidos en total ", len(datos), " bytes")
+print ("[CLIENTE] Datos recibidos en respuesta al CLIENTE: \"" + msg + "\"")
+
+
+if msg == MESSAGE:
+      print ("[CLIENTE] Conexión Establecida corrrectamente.")
+      print ("[CLIENTE] Cerrando conexion con el SERVIDOR")
+      s.close()
+
